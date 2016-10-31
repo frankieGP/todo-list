e5ec32cc60076f360c59199427e0b6c84865924e
Adam Baker and Frankie Gavin <frankie@>
2016-10-31 16:39:38 -0700

    working todo list with functional remove task buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -10,17 +10,15 @@ $(document).ready(function() {
   $("form#task-input").submit(function(event) {
     event.preventDefault();
 
-  var task = $("input#new-task").val();
+var task = $("input#new-task").val();
 
-  var newTask = new ToDo(task);
+var newTask = new ToDo(task);
 
-  $("ul#show-tasks").append("<li><span class ='task'>" + newTask.task + "</span><button type='submit' class='btn' id='remove'>Done!</button></li>");
-  debugger;
+$("ul#show-tasks").append("<li><span class ='task'>" + newTask.task + "<button type='submit' class='btn' id='remove'>DONZO!</button></span></li>");
 
-
-
-  $("#remove").on("click",function(){
-   $(this).closest("li").remove();
+$('#remove').on('click', function() {
+    $(this).closest('li').remove();
 });
+
   });
 });
